aedc1b443c6d9f369dfa431f2fba0810378ee788
kinopicofresh <kinopicofresh@gmail.com>
2025-04-17 11:16:03 +0800

    添加redisson 配置

---
+++ src/main/java/com/kinopio/flourmo/config/RedissonConfig.java
@@ -0,0 +1,34 @@
+package com.kinopio.flourmo.config;
+
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RedissonConfig {
+
+    @Value("${spring.data.redis.host}")
+    private String redisHost;
+
+    @Value("${spring.data.redis.port}")
+    private int redisPort;
+
+    @Value("${spring.data.redis.password:}")
+    private String redisPassword;
+
+    @Value("${spring.data.redis.database}")
+    private int redisDatabase;
+
+    @Bean
+    public RedissonClient redissonClient() {
+        Config config = new Config();
+        config.useSingleServer()
+                .setAddress("redis://" + redisHost + ":" + redisPort)
+                .setPassword(redisPassword.isEmpty() ? null : redisPassword)
+                .setDatabase(redisDatabase);
+        return Redisson.create(config);
+    }
+}
